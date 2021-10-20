dba6a8daa42d23e556c6a5279d30d739cd48b519
mzubair21 <mzubair21@outlook.com>
2021-10-21 02:08:48 +0500

    ballon pop game

---
+++ Ballom Pop Game/app.js
@@ -0,0 +1,30 @@
+let popped = 0;
+
+document.addEventListener('mouseover', function(e){
+    
+    if (e.target.className === "balloon"){
+        
+                e.target.style.backgroundColor = "#ededed";
+                e.target.textContent = "POP!";
+                popped++;
+				e.target.classList.remove("balloon");
+                removeEvent(e);
+                checkAllPopped();
+    }   
+});
+
+function removeEvent(e){
+    e.target.removeEventListener('mouseover', function(){
+        
+    })
+};
+
+function checkAllPopped(){
+    if (popped === 24){
+        console.log('all popped!');
+        let gallery = document.querySelector('#balloon-gallery');
+        let message = document.querySelector('#yay-no-balloons');
+        gallery.innerHTML = '';
+        message.style.display = 'block';
+    }
+};
