c335109de5df0eeed6da64f04dbde292b8056180
Sarwan Nizamani <sarwan.nizamani8@gmail.com>
2021-10-21 23:25:15 +0500

    add array functions

---
+++ Arrays Concept JavaScript/Array-functions.js
@@ -0,0 +1,57 @@
+/*This File Showes How To Create an Array In JavaScript and 
+
+what functions can be applied on Arrays
+
+*/
+
+
+
+/** 1: This is How You create an Array */
+
+let Names=['Aslam' , 'Ali' , 'Asadullah']
+
+///by doing console.log you will be able to see whole Array
+console.log(Names);
+
+/** You can see above you don't need any thing extra create the variable and assign the array */
+
+
+
+
+
+
+/* 2: How exactly do you check the length of an array?  */
+
+//Here is how
+
+console.log(Names.length);
+
+//Above console.log will output 3 , as we have declared our array with 3 values
+
+
+
+
+/* 3: How to access array values ? */
+
+/*
+
+    You can access value of an array  by passing Index of value you want to access 
+    Like If we want to access "ALI" we will need pass 1 to access ALI
+
+
+*/
+
+console.log(Names[1])
+
+//Above console log will output "Ali"
+
+
+
+
+
+
+
+//MORE FUNCTIONS WILL BE ADDED AS WELL SOON
+
+
+/* AUTHOR : https://github.com/sarwan920 */
